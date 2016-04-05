34140012214a5245ee61671a2aa896b9e6fe65ee
yeibercano <yeibercano@gmail.com>
2016-04-04 18:14:59 -0700

    Server now sends back parsable JSON

--- server/request-handler.js
+++ server/request-handler.js
@@ -42,7 +42,7 @@ var requestHandler = function(request, response) {
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
   response.writeHead(statusCode, headers);
-
+  response.end(JSON.stringify({foo:'bar'}));
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
   // response.end() will be the body of the response - i.e. what shows
@@ -50,7 +50,7 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end("Hello, World!");
+  // response.end("Hello, World!");
 
 };
 
